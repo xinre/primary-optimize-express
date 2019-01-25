3b6fc83ee9fa0f6d570685782b63609de3595d8a
tanghongyu <1336746261@qq.com>
2019-01-25 18:50:05 +0800

    12312

---
+++ test/index.js
@@ -0,0 +1,13 @@
+const express = require('../');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+app.get('/te', function (req, res) {
+    res.send('jijkjkj');
+});
+
+app.listen(3006, function () {
+    console.log('Example app listening on port 3006!');
+});
